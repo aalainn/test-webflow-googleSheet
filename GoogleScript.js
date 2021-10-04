6d49498cf97d769ef8d6ab825ad090e7dcbbdbce
Alain Gutjahr <a.gutjahr@swissinteractive.ch>
2021-10-04 13:44:06 +0200

    still trying to make WebApp to work. A bit close to the goal..

--- GoogleScript.js
+++ GoogleScript.js
@@ -1,26 +1,30 @@
 function doPost(e) {
-  // var lock = LockService.getScriptLock()
-  // lock.tryLock(10000)
+  console.log('doPost started...')
+  var lock = LockService.getScriptLock()
+  lock.tryLock(10000)
+
   
-  // Logger.log('Hello post')
+
+  return ContentService
+          .createTextOutput(JSON.stringify({ 'result': 'success', 'param' : e.parameter}))
+          .setMimeType(ContentService.MimeType.JSON)
 
   // var sheetName = 'Anfragen_via_Formular'
 
-  return ContentService.createTextOutput(JSON.stringify({ 'result': 'success'}))
-
   // try {
+  //   Logger.log('Try started...')
+
   //   var doc = SpreadsheetApp.openById('1PBcdmsADWXufwA4E6ZYqIkF4zI_3gE4E8bCokzxdd-4')
   //   var sheet = doc.getSheetByName(sheetName)
 
   //   var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-  //   Logger.log(headers)
   //   var nextRow = sheet.getLastRow() + 1
 
   //   /*var newRow = headers.map(function (header) {
   //     return header === 'Data' ? new Date() : e.parameter[header]
   //     });*/
   //     // var newRow = ['sdfffff', 'sdfsdf']
-  //     var newRow = []
+  //     // var newRow = []
   //     var newRow = [e.parameter.fname, e.parameter.lname]
 
       
@@ -31,11 +35,13 @@ function doPost(e) {
   //     .setMimeType(ContentService.MimeType.JSON)
   // }
   // catch (e) {
+  //   Logger.log('catch started...')
   //   return ContentService
   //     .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
   //     .setMimeType(ContentService.MimeType.JSON)
   // }
   // finally {
+  //   Logger.log('finally started...')
   //   lock.releaseLock()
   // }
   
